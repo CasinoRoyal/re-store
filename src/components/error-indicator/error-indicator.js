f98a85d5412c1d3474f3885e5f0c0ad6e6999a68
CasinoRoyal <lux_ex_tenebris@outlook.com>
2019-07-08 14:31:16 +0300

    created re-store project structure

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './error-indicator.css';
+
+const ErrorIndicator = () => {
+
+  return <div className="">Errror!!!!!</div>
+};
+
+export default ErrorIndicator;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return(
+    <div className="cssload-container">
+      <div className="cssload-whirlpool"></div>
+    </div>
+  );
+}
+
+export default Spinner;
